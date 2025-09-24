d4ba6d803b402e088082eea9d028a5bd5a23a5d8
LizenCerfalia <156718076+LizenCerfalia@users.noreply.github.com>
2025-09-23 20:15:51 -0400

    etc

--- labo2/scenes/projectile.gd
+++ labo2/scenes/projectile.gd
@@ -4,7 +4,7 @@ class_name Projectile
 var speed : int = 750
 
 func _physics_process(delta: float) -> void:
-	position += transform.x * speed * delta
+	position -= transform.y * speed * delta
 	
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	# On utilise mobs, mais cela peut être n'importe quel groupe

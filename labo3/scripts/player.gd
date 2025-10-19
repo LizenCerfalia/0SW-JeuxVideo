963cf51e987cc27e1de25a790a18b5610b22b68c
LizenCerfalia <156718076+LizenCerfalia@users.noreply.github.com>
2025-10-19 11:10:50 -0400

    input map : done

--- labo3/scripts/player.gd
+++ labo3/scripts/player.gd
@@ -4,7 +4,6 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_tree : AnimationTree = $AnimationTree
 
 var direction : float = 0.0
@@ -21,12 +20,13 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("ui_left", "ui_right")
+		
+	if Input.is_action_just_pressed("Killbind"):
+		get_tree().reload_current_scene()
+		
+	direction = Input.get_axis("Left", "Right")
 	if direction:
 		velocity.x = direction * SPEED
 		
@@ -38,7 +38,7 @@ func _physics_process(delta: float) -> void:
 func update_animation_parameters():
 	animation_tree.set("parameters/conditions/is_idle", velocity == Vector2.ZERO)
 	animation_tree.set("parameters/conditions/is_moving", velocity != Vector2.ZERO)
-	animation_tree.set("parameters/conditions/is_attacking", Input.is_action_just_pressed("attack"))
+	animation_tree.set("parameters/conditions/is_attacking", Input.is_action_just_pressed("Attack"))
 	
 	if(direction != 0.0):
 		animation_tree["parameters/Attack/blend_position"] = direction

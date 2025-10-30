f810bc23e52c25c5944a6b3240eecf210da13d25
LizenCerfalia <156718076+LizenCerfalia@users.noreply.github.com>
2025-10-30 13:39:59 -0400

    animation joueur : done

--- projetDeSession/projet-de-session/scripts/player_characters/knight.gd
+++ projetDeSession/projet-de-session/scripts/player_characters/knight.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
 @export var speed = 400.0
+@onready var sprite = $Sprite2D
 
 func _physics_process(delta: float) -> void:
 	var dir = Input.get_vector("P2_Left", "P2_Right","P2_Up","P2_Down").normalized()
 	
+	if (dir.x < 0.0):
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
+		
 	velocity = dir * speed
 	move_and_slide()
 	
--- projetDeSession/projet-de-session/scripts/player_characters/templar.gd
+++ projetDeSession/projet-de-session/scripts/player_characters/templar.gd
@@ -1,9 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 400.0
+@export var speed = 250.0
+@onready var sprite = $Sprite2D
+@onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
 	var dir = Input.get_vector("P1_Left", "P1_Right","P1_Up","P1_Down").normalized()
+	
+	if (dir != Vector2.ZERO):
+		animationPlayer.play("Walk_Side")
+	else:
+		animationPlayer.stop()
+	
+	if (dir.x < 0.0):
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
 	
 	velocity = dir * speed
 	move_and_slide()
